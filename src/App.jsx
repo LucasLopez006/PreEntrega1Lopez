01d157919f3071b547ae2f9563f80ad00c2db35b
Lucas López <llopeztechera@gmail.com>
2024-05-02 11:22:41 -0300

    Primer commit de React

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import NavBar from '../components/NavBar.jsx'
+
+function App(){ 
+
+  return (
+    
+    <div>
+      <NavBar/>
+    </div>
+
+  )
+
+}
+export default App
